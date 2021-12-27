dea902d979e925984385409850bbd89ccedea525
Dell XPS Ryan <ryanzulmi@gmail.com>
2021-12-27 14:40:15 +0700

    hapus eslintcache

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,21 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
+  const [title, setJudul] = useState(props.judul);
+
   const clickHandler = () => {
-    console.log('Clicked !!!');
+    setJudul('Update!');
   }
 
   return (
     <Card className='expense-item'>  
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
-        <h2>{props.judul}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
