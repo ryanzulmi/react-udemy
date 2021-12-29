3829da2ecd804ff29ca18913a87ee84d5e85b902
Dell XPS Ryan <ryanzulmi@gmail.com>
2021-12-29 16:05:22 +0700

    give conditional list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,26 @@
-import { useState } from 'react';
+//import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
-  const [title, setJudul] = useState(props.judul);
-
-  const clickHandler = () => {
-    setJudul('Update!');
-  }
+  // const [title, setJudul] = useState(props.judul);
+  // const clickHandler = () => {
+  //   setJudul('Update!');
+  // }
 
   return (
+    <li>
     <Card className='expense-item'>  
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.judul}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
+    </li>
   );
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,47 +1,25 @@
 import { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
     const expenses = props.expenses;
 
     const [filteredYear, setFilteredYear] = useState('2021');
-
+    
     const selectedYearHandler = (selectedYear) => {
-        setFilteredYear(selectedYear);
-    };
+        setFilteredYear(selectedYear); 
+    };  
+
+    const filteredExpenses = expenses.filter((expense) => expense.date.getFullYear().toString() === filteredYear);
+  
   return (
     <div>
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onSelectedYear={selectedYearHandler}/>
-      {expenses.map(
-        expenseItemList => <ExpenseItem 
-        judul={expenseItemList.title} 
-        amount={expenseItemList.amount} 
-        date={expenseItemList.date} 
-      />)}
-      {/* <ExpenseItem
-        judul={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        judul={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        judul={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        judul={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      /> */}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
     </div>
   );
